76c2286b7e982d5fa2fc01e9299f317d79d7a43b
Ramant <ramant@medhatech.in>
2025-05-29 12:25:54 +0530

    adding port in db connection

--- src/utils/dbconnection.util.ts
+++ src/utils/dbconnection.util.ts
@@ -7,6 +7,7 @@ const database = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
+        port:3307,
         dialect: 'mysql',
         //next two line help in debugging
         // logging: console.log,
